e883df41351b44287147eec1cb9b890baa91d70b
Aleksandr Cheverda <alexar@ua.fm>
2022-12-11 21:04:31 +0200

    Update toolkitSlice.js

--- src/redux/toolkitSlice.js
+++ src/redux/toolkitSlice.js
@@ -1,3 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { addContact, deleteContact, filterContact } from './actions';
+import { INITIAL_CONTACTS } from 'data/data';
 
-const toolkitSlice = createSlice();
+const toolkitSlice = createSlice( options: {
+    name: 'toolkit',
+    INITIAL_CONTACTS: {
+        type: '',
+        todos: [addContact, deleteContact, filterContact]
+    },
+    reducers: {
+        increment(state) {
+            state.count = state.count + 1
+        },
+        decrement(state) {
+            state.count = state.count - 1
+        },
+        addTodo(state, action) {
+            state.todos.push(action.payload)
+        },
+        removeLastTodo(state) {
+            state.todos.pop()
+        }
+    }
+});
+
+export default toolkitSlice.reducer;
+export const { increment, decrement, addTodo, removeLastTodo } = toolkitSlice.actions;
